3b605dfadb3b9a6adf672d1817367066eb2c5a81
SoUrAv <aansourav@gmail.com>
2024-02-16 11:06:24 +0600

    Global Error Page Added

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import ErrorPage from "./Error";
 import Root from "./Root";
 import "./index.css";
 
@@ -8,6 +9,7 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
+    errorElement: <ErrorPage />,
   },
 ]);
 
